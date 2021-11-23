dd748a1756fca7272b9fec70ef36bb484a623408
salihkoc <salihkoc-e@users.noreply.github.com>
2021-11-23 18:36:01 +0300

    Add project files.

---
+++ recursiveFonksiyonlar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recursiveFonksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                long sayiFaktoriyel = faktoriyel(i);
+                Console.WriteLine($"{i} sayısının faktoriyeli "+sayiFaktoriyel);
+            }
+
+            Console.ReadLine();
+        }
+
+        static long faktoriyel(long sayac)
+        {
+
+            if (sayac<=1)
+            {
+                return 1;
+            }
+            return sayac * faktoriyel(sayac - 1);
+        }
+    }
+}
